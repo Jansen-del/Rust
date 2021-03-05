1ec67c134ee5089fe9300603c3e4a357cbd6ad1b
Jansen George <jansen.george@usask.ca>
2021-03-04 22:08:37 -0500

    use underscore to catch all missed cases

--- section4/enums/src/main.rs
+++ section4/enums/src/main.rs
@@ -8,7 +8,17 @@ enum Color{
     rgbColor
 }
 
+fn enums() {
+    let c:Color = Color::Red;
+    match c {
+        Color::Red => println!("R"),
+        Color::Green => println!("G"),
+        Color::Blue => println!("B"),
+        _ => println!("who knows what this is")
+    }
+}
 
 fn main() {
     println!("Hello, world!");
+    enums();
 }
