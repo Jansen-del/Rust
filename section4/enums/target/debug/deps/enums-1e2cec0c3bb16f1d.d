957251b4dd12ad555255c2d70e691cf2e1ec74b6
Jansen George <jansen.george@usask.ca>
2021-03-04 21:58:14 -0500

    created enums project to study it

---
+++ section4/enums/target/debug/deps/enums-1e2cec0c3bb16f1d.d
@@ -0,0 +1,5 @@
+/home/pi/RustFiles/section4/enums/target/debug/deps/enums-1e2cec0c3bb16f1d: src/main.rs
+
+/home/pi/RustFiles/section4/enums/target/debug/deps/enums-1e2cec0c3bb16f1d.d: src/main.rs
+
+src/main.rs:
---
+++ section4/enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+/home/pi/RustFiles/section4/enums/target/debug/enums: /home/pi/RustFiles/section4/enums/src/main.rs
