dd870d68c0eb1a3f12ddd0ef7be78713f3df2c3b
Jansen George <jansen.george@usask.ca>
2021-03-04 21:38:52 -0500

    learning to create and use structs

--- section4/dataStructure/src/main.rs
+++ section4/dataStructure/src/main.rs
@@ -1,5 +1,28 @@
 #![allow(non_snake_case)]
+#![allow(dead_code)]
+
+//creating a struct/dictionary structure to store GPS coordinates by default
+struct Point {
+    x:f64,
+    y:f64
+}
+
+struct  Line{
+    start:Point,
+    end:Point
+}
+
+fn structures(){
+    let p1 = Point {x:3.0, y:4.0};
+    println!("point p is a ({}, {}) coordintaes", p1.x, p1.y);
+    let p2 = Point {x:15.0, y:-79.0};
+    println!("Point p2 is ({}, {}) coordinates ", p2.x, p2.y)
+    let myLine = Line {start:p1, end:p2}
+}
+
+
 
 fn main() {
     println!("Hello, world!");
+    structures()
 }
