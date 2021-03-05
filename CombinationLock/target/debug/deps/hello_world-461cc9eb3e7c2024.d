7d30f8c842f26ad6461bdc1c2cbb125c5690acac
Jansen George <jansen.george@usask.ca>
2021-03-04 21:14:40 -0500

    created new Folder to learn data structures in rust

--- CombinationLock/target/debug/deps/hello_world-461cc9eb3e7c2024.d
+++ CombinationLock/target/debug/deps/hello_world-461cc9eb3e7c2024.d
@@ -1,5 +1,5 @@
-/home/pi/RustFiles/hello_world/target/debug/deps/hello_world-461cc9eb3e7c2024: src/main.rs
+/home/pi/RustFiles/CombinationLock/target/debug/deps/hello_world-461cc9eb3e7c2024: src/main.rs
 
-/home/pi/RustFiles/hello_world/target/debug/deps/hello_world-461cc9eb3e7c2024.d: src/main.rs
+/home/pi/RustFiles/CombinationLock/target/debug/deps/hello_world-461cc9eb3e7c2024.d: src/main.rs
 
 src/main.rs:
--- CombinationLock/target/debug/hello_world.d
+++ CombinationLock/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/home/pi/RustFiles/hello_world/target/debug/hello_world: /home/pi/RustFiles/hello_world/src/main.rs
+/home/pi/RustFiles/CombinationLock/target/debug/hello_world: /home/pi/RustFiles/CombinationLock/src/main.rs
