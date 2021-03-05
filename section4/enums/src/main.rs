0566be8294bb35048cdf93eb0e6bd769da50b6a1
Jansen George <jansen.george@usask.ca>
2021-03-04 22:14:38 -0500

    enums completed

--- section4/enums/src/main.rs
+++ section4/enums/src/main.rs
@@ -5,16 +5,19 @@ enum Color{
     Red, 
     Green, 
     Blue, 
-    rgbColor
+    rgbColor (u8, u8, u8), 
+    cmyk {cyan:u8, magenta: u8, yellow: u8, black: u8}
 }
 
 fn enums() {
-    let c:Color = Color::Red;
+    let c:Color = Color::cmyk{cyan:3, magenta:23, yellow:234, black:0};
     match c {
         Color::Red => println!("R"),
         Color::Green => println!("G"),
         Color::Blue => println!("B"),
+        Color::rgbColor (0,0,0) => println!("black"),
         _ => println!("who knows what this is")
+        
     }
 }
 
