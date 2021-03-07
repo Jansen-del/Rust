b30ced2cb5201e4d239c1afb9195996c9cab2e21
Jansen-del <jansen.george@usask.ca>
2021-03-07 01:29:35 +0000

    testing option types used keywords some and none Not sure when I woudl ever need to use this

--- section4/rough/src/main.rs
+++ section4/rough/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let x =  3.0;
+    let y = 1.0;
+
+    //option  
+    let result = 
+        if y!= 0.0 {Some(x/y)} else {None};
+    // println!{"result is {}", result}
+    match result {
+        Some(z) => println!("{}/{}={}",x, y, z),
+        None => println!("cannot divide by zero")
+    }
 }
