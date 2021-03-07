9a19358dbdb512ee93e14089c9aff46d4e0a894a
Jansen-del <jansen.george@usask.ca>
2021-03-07 01:21:49 +0000

    creating folder for rough code

---
+++ section4/rough/target/debug/deps/rough-1e3563264110ed17.d
@@ -0,0 +1,5 @@
+/home/pi/Rust/section4/rough/target/debug/deps/rough-1e3563264110ed17: src/main.rs
+
+/home/pi/Rust/section4/rough/target/debug/deps/rough-1e3563264110ed17.d: src/main.rs
+
+src/main.rs:
---
+++ section4/rough/target/debug/rough.d
@@ -0,0 +1,1 @@
+/home/pi/Rust/section4/rough/target/debug/rough: /home/pi/Rust/section4/rough/src/main.rs
