509960a3c73046e06bb7a0cb4165a9e706df5e00
Jansen-del <jansen.george@usask.ca>
2021-03-12 23:51:13 +0000

    pattern matching test

--- section4/patternmatching/src/main.rs
+++ section4/patternmatching/src/main.rs
@@ -1,3 +1,15 @@
+fn how_many(x:i32) -> &`static str{
+    match x {
+        0 => "no"
+    }
+}
+
 pub fn pattern_matching(){
-    
+    for x in 1..13{
+        println!("{}: I have {} orange(s)", x, how_many(x));
+    }    
+}
+
+fn main() {
+    pattern_matching()
 }
