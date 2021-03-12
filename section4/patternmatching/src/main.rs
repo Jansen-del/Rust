e2ebcc0a0a8dbcd991408fd8a82204652d4eb2e8
Jansen-del <jansen.george@usask.ca>
2021-03-12 23:57:39 +0000

    learned to use multipel options in match

--- section4/patternmatching/src/main.rs
+++ section4/patternmatching/src/main.rs
@@ -1,12 +1,17 @@
-fn how_many(x:i32) -> &`static str{
+fn how_many(x:i32) -> &'static str{
     match x {
-        0 => "no"
+        0 => "no",
+        1|2 => "a couple of",
+        12 => "a dozen",
+        9..=11 => "lots of",
+        _ => "a few"
+
     }
 }
 
 pub fn pattern_matching(){
-    for x in 1..13{
-        println!("{}: I have {} orange(s)", x, how_many(x));
+    for x in 0..13{
+        println!("{}: I have {} oranges", x, how_many(x));
     }    
 }
 
